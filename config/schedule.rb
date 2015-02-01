dd487a64ae7755d468d52f91e4aaf928cc515b88
Nate Sullivan <nate@academia.edu>
2015-01-31 18:35:03 -0800

    Change desktop background every five minutes

---
+++ config/schedule.rb
@@ -0,0 +1,6 @@
+set :output, error: '/var/log/arium-cron-error.log', standard: '/var/log/arium-cron.log'
+dir = File.expand_path('..', File.expand_path(File.dirname(__FILE__)))
+
+every 5.minutes do
+  command "cd #{dir} && bin/random_desktop"
+end
