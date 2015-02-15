fe2376a6991f78bc962e459965b2b5b9ff573063
Nate Sullivan <nate@academia.edu>
2015-02-14 20:23:53 -0800

    Display village world on desktop

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, error: '/var/log/arium-cron-error.log', standard: '/var/log/arium-cron.log'
 dir = File.expand_path('..', File.expand_path(File.dirname(__FILE__)))
 
-every 10.minutes do
+every 5.minutes do
   command "cd #{dir} && bundle exec bin/arium_desktop"
 end
