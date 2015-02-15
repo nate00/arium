3db2ac3be4f3ca5f2c18eb6794b72d60ef858deb
Nate Sullivan <nate@academia.edu>
2015-02-14 20:09:08 -0800

    Remove empty initialize method

--- lib/arium/evolvers/example.rb
+++ lib/arium/evolvers/example.rb
@@ -1,9 +1,6 @@
 module Arium
   module Evolvers
     class Example
-      def initialize
-      end
-
       def evolve(previous)
         row = previous.sample
         row[Kernel.rand(row.size)] = :mountain
