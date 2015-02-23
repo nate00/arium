a53583d18cda2c010183ea05dbf27f3b566bf8da
Nate Sullivan <nate@academia.edu>
2015-02-22 22:58:21 -0800

    Add general-purpose World#generation
    World#generation calls #first_generation or #next_generation as
    appropriate.

--- lib/arium/world.rb
+++ lib/arium/world.rb
@@ -3,23 +3,44 @@ require 'JSON'
 module Arium
   class World
     include Persistence
+    include Configurable
 
-    def initialize(creator, evolvers)
-      @creator = creator
-      @evolvers = evolvers
-    end
+    config_accessor :creator, :evolvers
+
+    # Config:
+    #   creator
+    #   evolvers
+    #   generation_locator
 
     def first_generation(outfile)
-      generation = @creator.create
+      generation = creator.create
       write_generation(outfile, generation)
+      outfile
     end
 
     def next_generation(infile, outfile)
       next_gen = 
-        @evolvers.inject(read_generation(infile)) do |prev, evolver|
+        evolvers.inject(read_generation(infile)) do |prev, evolver|
           evolver.evolve(prev)
         end
       write_generation(outfile, next_gen)
+      outfile
+    end
+
+    def generation(number)
+      outfile = locate_generation(number)
+      if number == 0
+        first_generation(outfile)
+      else
+        infile = locate_generation(number - 1)
+        next_generation(infile, outfile)
+      end
+    end
+
+    private
+
+    def locate_generation(number)
+      config.generation_locator.call(number)
     end
   end
 end
